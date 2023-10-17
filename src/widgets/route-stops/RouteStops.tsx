fbbf55726cac5f77b7c0f191b89f14e26701191a
Dmitry <arhidiman@mail.ru>
2023-10-17 18:02:47 +0700

    добавлена зависимость количества отображаемых остановок от статуса движения транспорта (на стоянке или при движении), добавлен градиент последней линии остановки

--- src/widgets/route-stops/RouteStops.tsx
+++ src/widgets/route-stops/RouteStops.tsx
@@ -1,14 +1,40 @@
+import { useState, useEffect } from "react"
 import "./RouteStops.scss"
 import RouteItem from "./route-item/RouteItem"
 import { route } from "src/app/mock-data/route"
 
-const stops = route.stops
+
 
 function RouteStops() {
 
+    const stops = route.stops
+    const inMove = false
+    let count = 0
+
+    const [displayedStops, setDisplayedStops] = useState(4)
+
+    useEffect(() => {
+        inMove ? setDisplayedStops(4) : setDisplayedStops(3) 
+    }, [inMove])
+
+
     return (
         <div className="route-stops">
-            {stops.map((stop, i)=> <RouteItem nameRus={stop.nameRus} nameEng={stop.nameEng} timeLeft={i*5}/>)}
+            {
+                stops.map((stop, i)=> {
+                console.log(i, count)
+                    if(count < displayedStops) {
+                        count +=1
+                        return <RouteItem 
+                            className={i === displayedStops - 1 ? 'line-fade' : ""}
+                            inMove={inMove}
+                            nameRus={stop.nameRus} 
+                            nameEng={stop.nameEng} 
+                            timeLeft={3+i*5}
+                        />
+                    }
+                })
+            }
         </div>
     )
 }
--- src/widgets/route-stops/route-item/RouteItem.tsx
+++ src/widgets/route-stops/route-item/RouteItem.tsx
@@ -1,18 +1,20 @@
 interface IRouteItem {
+    className?: string,
+    inMove: boolean,
     nameRus: string,
     nameEng: string,
-    timeLeft: number
+    timeLeft: number,
 }
 
-function RouteItem({nameRus, nameEng, timeLeft}: IRouteItem) {
+function RouteItem({className, inMove, nameRus, nameEng, timeLeft}: IRouteItem) {
 
     return (
-    <div className="route-item">
+    <div className={`route-item ${inMove ? 'in-move' : ""}`}>
         <span className="route-item__time-left">{timeLeft}</span>
         <div className="route-item__route-line">
             <span className="route-item__route-line_top"></span>
             <div className="route-item__route-line_circle"></div>
-            <span className="route-item__route-line_bottom"></span>
+            <span className={`route-item__route-line_bottom ${className && className}`}></span>
         </div>
         
         <div className="route-item__stop-name">
