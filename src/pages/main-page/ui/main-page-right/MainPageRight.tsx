56b60a7db6a9090f34c5644f6132f5c02fd541e5
Архипов Дмитрий <Arkhipov.Dmitrii@INNOVACIA.LOCAL>
2023-10-18 13:13:25 +0300

    добавлены преходы экранов в правой части приложения

--- src/pages/main-page/ui/main-page-right/MainPageRight.tsx
+++ src/pages/main-page/ui/main-page-right/MainPageRight.tsx
@@ -1,13 +1,38 @@
 import "./MainPageRight.scss"
+import { useEffect, useState,  } from "react"
 import contentTemplate from "src/app//images/content-template.png"
 import FlightsInfo from "src/widgets/airport-info/ui/FlightsInfo"
 import ReceptionDesksScheme from "src/widgets/reception-desks-scheme/ui/ReceptionDesksScheme"
 
+const screens = [
+  <img src={contentTemplate} alt="currnet place content"/>,
+  <FlightsInfo/>,
+  <ReceptionDesksScheme/>
+]
+
 function MainPageRight() {
+
+    const [iscreenInterval, setScreenInterval] = useState(null)
+    const [screenNum, setScreenNum] = useState(0)
+
+    useEffect(() => {
+      const screensTotal = screens.length
+      const interval = setTimeout(() => {
+
+        if(screenNum === screensTotal - 1) {
+          setScreenNum(0)
+        } else setScreenNum(screenNum + 1)
+      },4000)
+
+      setScreenInterval(interval)
+      return () => clearInterval(interval)
+    }, [screenNum])
+
+    console.log(screenNum)
     return (
     
     <div className="main-page-right">
-      <img src={contentTemplate} alt="currnet place content"/>
+      {screens[screenNum]}
        {/* <FlightsInfo/>
        <ReceptionDesksScheme/> */}
     </div>
